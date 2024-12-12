07c6e018e5c77459becc1fd46ab6238e142d3124
smnnmn <leejs899797@gmail.com>
2024-12-12 21:44:04 +0900

    {2024.12.12} 거품 정렬

---
+++ Class2th (Brute Force)/Source.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#pragma region 브루트 포스
+	// 가능한 모든 경우의 수를 탐색하면서
+	// 결과를 도출하는 알고리즘입니다.
+	// (무식하게 힘으로 밀어붙히는 거라고 생각하면 됨)
+
+	int password[3] = { 3,4,5 };
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			for (int k = 0; k < 10; k++)
+			{
+				if (i == password[0] && j == password[1] && k == password[2])
+				{
+					cout << "해결!!" << endl;
+					cout << "비밀번호는: " <<
+						password[0] << password[1] << password[2] << endl;
+				}
+			}
+		}
+	}
+
+#pragma endregion
+
+
+	return 0;
+}
---
+++ Class3th (Bubble Sort)/Source.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#define SIZE 5
+
+int main()
+{
+#pragma region 거품 정렬
+	// 서로 인접한 두 원소를 비교하여 정렬하는 알고리즘입니다.
+
+	int list[SIZE] = { 4,1,3,2,5 };
+	for (int i = 0; i < SIZE - 1; i++)
+	{
+		for (int j = 0; j < (SIZE - 1 - i); j++)
+		{
+			if (list[j] > list[j + 1])
+			{
+				swap(list[j], list[j + 1]);
+			}
+		}
+	}
+
+
+
+	cout << "결과" << endl;
+	for (const int& element : list) // const로 읽기 전용, & 로 복사비용 X
+	{
+		cout << element << " ";
+	}
+
+#pragma endregion
+
+	return 0;
+}
