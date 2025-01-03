10cfc3947576263b3ad366177533c7eb43894723
smnnmn <leejs899797@gmail.com>
2025-01-03 21:45:03 +0900

    {2025.1.3} 다익스트라 알고리즘

---
+++ Class20th (Minimum Spanning Tree)/Source.cpp
@@ -0,0 +1,156 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+#define SIZE 8
+
+using namespace std;
+
+class Graph
+{
+private:
+	class Edge
+	{
+	private:
+		int x;
+		int y;
+		int distance;
+
+	public:
+		Edge(int x, int y, int distance)
+		{
+			this->x = x;
+			this->y = y;
+			this->distance = distance;
+		}
+
+		const int& X()
+		{
+			return x;
+		}
+
+		const int& Y()
+		{
+			return y;
+		}
+
+		const int& Distance()
+		{
+			return distance;
+		}
+
+		const bool& operator < (const Edge& edge)
+		{
+			return distance < edge.distance;
+		}
+	};
+
+	std::vector<Edge> graph;
+
+	int cost;
+	int parent[SIZE];
+
+public:
+	Graph()
+	{
+		cost = 0;
+
+		for (int i = 0; i < SIZE; i++)
+		{
+			parent[i] = i;
+		}
+	}
+
+	void Insert(int x, int y, int distance)
+	{
+		graph.push_back(Edge(x, y, distance));
+	}
+
+	void Kruskal()
+	{
+		sort(graph.begin(), graph.end());
+
+		for (int i = 0; i < graph.size(); i++)
+		{
+			if (Same(graph[i].X(), graph[i].Y()) == false)
+			{
+				Union(graph[i].X(), graph[i].Y());
+				cost += graph[i].Distance();
+			}
+		}
+
+		cout << "Graph 최소 비용 : " << cost << endl;
+
+	}
+	int Find(int x)
+	{
+		if (x == parent[x])
+		{
+			return x;
+		}
+		else
+		{
+			return parent[x] = Find(parent[x]);
+		}
+	}
+
+	void Union(int x, int y)
+	{
+		x = Find(x);
+		y = Find(y);
+
+		if (x < y)
+		{
+			parent[y] = x;
+		}
+		else
+		{
+			parent[x] = y;
+		}
+	}
+
+	bool Same(int x, int y)
+	{
+		return Find(x) == Find(y);
+	}
+
+};
+
+int main()
+{
+#pragma region 신장 트리
+	// 그래프의 모든 정점을 포함하면서 사이클이 존재하지 않는
+	// 부분 그래프로, 그래프의 모든 정점을 최소 비용으로 연결하는 트리입니다.
+
+	// 그래프의 정점의 수가 n개일 때, 간선의 수는 n-1개 입니다.
+
+	// 최소 비용 신장 트리
+	// 그래프의 간선들의 가중치 합이 최소인 신장 트리
+
+	Graph graph;
+
+	graph.Insert(1, 7, 12);
+	graph.Insert(4, 7, 13);
+
+	graph.Insert(1, 4, 28);
+	graph.Insert(2, 4, 24);
+
+	graph.Insert(1, 2, 67);
+	graph.Insert(1, 5, 17);
+
+	graph.Insert(5, 7, 73);
+	graph.Insert(2, 5, 64);
+
+	graph.Insert(5, 3, 20);
+	graph.Insert(5, 6, 40);
+	graph.Insert(3, 6, 35);
+
+	graph.Kruskal();
+
+#pragma endregion
+
+
+	return 0;
+}
+
+
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,146 +1,61 @@
 ﻿#include <iostream>
-#include <algorithm>
-#include <vector>
-
-#define SIZE 8
 
 using namespace std;
+
+#define SIZE 6
+#define INFINITY 10000000
 
 class Graph
 {
 private:
-	class Edge
+	int weight[SIZE][SIZE] =
 	{
-	private:
-		int x;
-		int y;
-		int distance;
-
-	public:
-		Edge(int x, int y, int distance)
-		{
-			this->x = x;
-			this->y = y;
-			this->distance = distance;
-		}
-
-		const int & X()
-		{
-			return x;
-		}
-
-		const int & Y()
-		{
-			return y;
-		}
-
-		const int & Distance()
-		{
-			return distance;
-		}
-
-		const bool & operator < (const Edge & edge)
-		{
-			return distance < edge.distance;
-		}
+		{0, 2, 5, 1, INFINITY, INFINITY},
+		{2, 0, 3, 2, INFINITY, INFINITY},
+		{5, 3, 0, 3,        1,        5},
+		{1, 2, 3, 0,        1, INFINITY},
+		{INFINITY,INFINITY,1,1,0,2},
+		{INFINITY,INFINITY,5,INFINITY,2,0},
 	};
-
-	std::vector<Edge> graph;
-
-	int cost;
-	int parent[SIZE];
+	bool visited[SIZE];
+	int distance[SIZE];
 
 public:
 	Graph()
 	{
-		cost = 0;
-
 		for (int i = 0; i < SIZE; i++)
 		{
-			parent[i] = i;
+			visited[i] = false;
+			distance[i] = 0;
 		}
-	}
-
-	void Insert(int x, int y, int distance)
-	{
-		graph.push_back(Edge(x, y, distance));
-	}
-
-	void Kruskal()
-	{
-		sort(graph.begin(), graph.end());
-
-		for (int i = 0; i < graph.size(); i++)
-		{
-			cout << graph[i].X() << " " << graph[i].Y() << " " << graph[i].Distance() << endl;
-		}
-	}
-	int Find(int x)
-	{
-		if (x == parent[x])
-		{
-			return x;
-		}
-		else
-		{
-			return parent[x] = Find(parent[x]);
-		}
-	}
-
-	void Union(int x, int y)
-	{
-		x = Find(x);
-		y = Find(y);
-
-		if (x < y)
-		{
-			parent[y] = x;
-		}
-		else
-		{
-			parent[x] = y;
-		}
-	}
-
-	bool Same(int x, int y)
-	{
-		return Find(x) == Find(y);
 	}
 };
 
 int main()
 {
-#pragma region 신장 트리
-	// 그래프의 모든 정점을 포함하면서 사이클이 존재하지 않는
-	// 부분 그래프로, 그래프의 모든 정점을 최소 비용으로 연결하는 트리입니다.
+#pragma region 다익스트라 알고리즘
+	// 시작점으로부터 모든 노드까지의 최소 거리를 구해주는 
+	// 알고리즘입니다.
 
-	// 그래프의 정점의 수가 n개일 때, 간선의 수는 n-1개 입니다.
+	// 1. distance 배열에 weight[시작점 노드]의 값들로
+	//	  초기화 합니다.
 
-	// 최소 비용 신장 트리
-	// 그래프의 간선들의 가중치 합이 최소인 신장 트리
+	// 2. 시작점을 방문 처리합니다.
+
+	// 3. distance 배열에서 최소 비용 노드를 찾고 방문 처리합니다.
+	//	  단, 이미 방문한 노드는 제외합니다.
+
+	// 4. 최소 비용 노드를 거쳐갈 지 고민해서 distance 배열을 갱신합니다.
+	//	  단, 이미 방문한 노드는 제외합니다.
+
+	// 5. 모든 노드를 방문할 때까지 3번 ~ 4번을 반복합니다.
+
+	// 방문하지 않은 노드 중에서 가장 작은 distance를 가진 노드를
+	// 방문하고, 그 노드와 연결된 다른 노드까지의 거리를 계산합니다.
 
 	Graph graph;
 
-	graph.Insert(1, 7, 12);
-	graph.Insert(4, 7, 13);
-
-	graph.Insert(1, 4, 28);
-	graph.Insert(2, 4, 24);
-
-	graph.Insert(1, 2, 67);
-	graph.Insert(1, 5, 17);
-
-	graph.Insert(5, 7, 73);
-	graph.Insert(2, 5, 64);
-
-	graph.Insert(5, 3, 20);
-	graph.Insert(5, 6, 40);
-	graph.Insert(3, 6, 35);
-
-	graph.Kruskal();
-
 #pragma endregion
-
 
 	return 0;
 }
